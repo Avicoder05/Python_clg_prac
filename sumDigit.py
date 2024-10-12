6cd15679b03b6472d8430c2ee0a660eac9314e9d
Avicoder05 <vkumarbharti3@gmail.com>
2024-10-13 00:32:42 +0530

    modify

--- sumDigit.py
+++ sumDigit.py
@@ -1,8 +1,11 @@
 #WAP to calculate sum of digit of an integer between 1 to 1000
-number = 566
+
+
+number=int(input("Enter number = "))
 digit1=number//100
 remainder1=(number%100)
 digit2=remainder1//10
 digit3=remainder1%10
 sum = digit1+digit2+digit3
-print(sum)
+print("The number whose digits need to be summed is : ",number)
+print("Sum of digit of given number is : ",sum)
