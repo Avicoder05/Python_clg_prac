c1ef8ae354db72f7f3f9067f5f75779079505165
Avicoder05 <vkumarbharti3@gmail.com>
2024-10-15 22:22:49 +0530

    new file

---
+++ conversion.py
@@ -0,0 +1,5 @@
+#number conversion
+num=int(input("Enter the number = "))
+print("Number = ",num,"Binary equivalent = ", bin(num))
+print("Number = ",num,"Octal equivalent = ", oct(num))
+print("Number = ",num,"Hexadecimal equivalent = ", hex(num))
